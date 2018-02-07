891fa48fcb3265163437b8bdec3895406c17b237
Kevin Wilson <kwilson@scottishpower.com>
2018-02-07 02:17:29 +0000

    Old file cleanup

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Builder<T extends object> {
-    private instance;
-    private withoutProperties;
-    static create<T extends object>(instance: T): default<T>;
-    private constructor();
-    with(property: keyof T, value: any): Builder<T>;
-    without(...properties: Array<keyof T>): Builder<T>;
-    build(): T;
-}
